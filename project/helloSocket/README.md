97144c2a80462124f2a476f888ee62772e4e6a27
1904240502 <zhangwei7234@gmail.com>
2023-02-16 20:47:17 +0800

    add midTcp project

--- project/helloSocket/README.md
+++ project/helloSocket/README.md
@@ -11,11 +11,33 @@
 | TCP服务端  | TCP客户端 | 
 | :-----:| :----: | 
 | 建立socket | 建立socket | 
-| 绑定端口bind | 连接服务器connect | 
-|监听listen| 接受消息recv|
-|接受连接accept|关闭socket closesocket|
-|发送消息send||
-|关闭socket||
-|[服务端代码]()|[客户端代码]()|
+| 绑定端口bind |  | 
+|监听listen| 连接服务器connect|
+|接受连接accept||
+|发送消息send|接受消息recv|
+|关闭socket|关闭socket closesocket|
+|[服务端代码](https://github.com/1904240502/c-with-class/blob/project/project/EasyTcpServer/server.cpp)|[客户端代码](https://github.com/1904240502/c-with-class/blob/project/project/EastyTcpclient/client.cpp)|
+***
+
+### *** 建立能持续请求的c/s网络程序 ***
+
+```
+服务端：等待新客户端连接，接受客户端请求，处理请求后发送消息给客户端
+客户端：连接服务端，输入请求，向服务端发送请求，接受服务端处理后的消息
+```
+
+| TCP服务端  | TCP客户端 | 
+| :-----:| :----: | 
+| 建立socket | 建立socket | 
+| 绑定端口bind |  | 
+|监听listen| 连接服务器connect|
+|接受连接accept|客户输入请求cin|
+||处理请求strcmp|
+||向服务端发送请求send|
+|接受客户端请求recv||
+|处理请求strcmp||
+|向客户发送消息send|接受消息recv|
+|关闭socket|关闭socket closesocket|
+|[服务端代码](https://github.com/1904240502/c-with-class/blob/project/project/midTcpServer/server.cpp)|[客户端代码](https://github.com/1904240502/c-with-class/blob/project/project/midTcpclient/client.cpp)|
 ***
 
---
+++ project/midTcpServer/server.cpp
@@ -0,0 +1,90 @@
+#define WIN32_LEAN_AND_MEAN  //解决头文件重定义
+#define _WINSOCK_DEPRECATED_NO_WARNINGS //inet_ntoa 
+
+#include<Windows.h>
+#include<WinSock2.h>
+#include<iostream>
+
+#pragma comment(lib,"ws2_32.lib") //windows 附加依赖项
+
+int main()
+{
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+
+	//建立socket套节字
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (INVALID_SOCKET == sock)
+		std::cout << "服务端socket创建成功！" << std::endl;
+	else
+		std::cout << "服务端socket创建成功！" << std::endl;
+
+	//绑定客户端网络端口
+	sockaddr_in sin = {};
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(4567);
+	sin.sin_addr.S_un.S_addr = INADDR_ANY;//inet_addr("127.0.0.1");
+	if (SOCKET_ERROR == bind(sock, (sockaddr*)&sin, sizeof(sockaddr_in)))
+		std::cout << "绑定端口失败！" << std::endl;
+	else
+		std::cout << "绑定端口成功！" << std::endl;
+
+	//监听listen
+	if (SOCKET_ERROR == listen(sock, 5))
+		std::cout << "服务端监听失败！" << std::endl;
+	else
+		std::cout << "服务端监听成功！" << std::endl;
+
+	sockaddr_in clientAddr = {};
+	int addrlen = sizeof(clientAddr);
+	SOCKET cSock = INVALID_SOCKET;
+
+	char reBuf[1024] = {};
+	int rlen = 0;
+
+	//接受客户端连接accept
+	cSock = accept(sock, (sockaddr*)&clientAddr, &addrlen);
+	if (INVALID_SOCKET == cSock)
+		std::cout << "接受到客户端失败！" << std::endl;
+	else
+		std::cout << "接受到客户端成功！" << std::endl;
+
+
+	while (true)
+	{
+		//接受客户端请求recv
+		rlen = recv(cSock, reBuf, 1024, 0);
+		if (rlen <= 0)
+			std::cout << "客户端--接受消息失败！" << std::endl;
+		else {
+			std::cout << "接受到服务端消息！" << std::endl;
+			std::cout << "要处理的请求:" << reBuf << std::endl;
+		}
+
+		//消息处理
+		if (0 == strcmp(reBuf, "getname"))
+		{
+			//向客户端发送数据send
+			char sBuf[] = "zhang";
+			if (0 >= send(cSock, sBuf, strlen(sBuf) + 1, 0))
+				std::cout << "向客户端发送消息失败！" << std::endl;
+			else
+				std::cout << "向客户端发送消息成功！" << std::endl;
+		}
+		else {
+			//向客户端发送数据send
+			char sBuf[] = "???";
+			if (0 >= send(cSock, sBuf, strlen(sBuf) + 1, 0))
+				std::cout << "向客户端发送消息失败！" << std::endl;
+			else
+				std::cout << "向客户端发送消息成功！" << std::endl;
+		}
+	}
+
+	//关闭服务端closesocket
+	closesocket(sock);
+
+	WSACleanup();
+	return 0;
+}
